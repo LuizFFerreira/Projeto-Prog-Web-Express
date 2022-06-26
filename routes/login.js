36ec6c4a149500fd3f8e1651b6e345010cffe2aa
LuizFFerreira <93801358+LuizFFerreira@users.noreply.github.com>
2022-06-26 14:40:18 -0300

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,16 @@
 var express = require("express");
 var router = express.Router();
 var User = require("../models/User");
+var sha256 = require('js-sha256');
+var session = require('express-session');
+
+session({secret: "Your secret key",
+                  resave: false,
+                  saveUninitialized: false,
+                  cookie: {
+                     expires: 600000,
+                  },
+});
 
 router.get('/', function(req, res){
     res.render('login');
@@ -8,10 +18,19 @@ router.get('/', function(req, res){
 
 router.post("/", async function(req, res){
     var record = await User.findOne(req.body.username);
+    var hSenha = sha256(req.body.senha).toString();
+    var senhaCorreta = await User.findUserSenha(req.body.username, hSenha);
+
     if(!record){
-        console.log("nao existe")
-    } else{
-        res.send(record);
+        console.log("usuário nao existe")
+        res.redirect('/login');
+    } else if(senhaCorreta) {
+        req.session.user = record;
+        req.session.user.username = req.body.username;
+
+        res.redirect('/');
+    }else{
+        res.redirect('/login');
     }
 })
 
